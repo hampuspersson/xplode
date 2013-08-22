cce063415b20f58d41297cdf60d808feef525cc8
Hampus Persson <hampus@hampuspersson.se>
2013-08-22 13:51:55 +0200

    Enabling user login/logout
    Resolves #1

--- app/routes.php
+++ app/routes.php
@@ -1,22 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+/*-------------------------------------------------------------------------
+| Standard routes
+|------------------------------------------------------------------------*/
 
 Route::get('/', array( 'before' => 'auth', function() {
-	return View::make('dashboard');
+	return View::make('home');
 }));
 
 Route::get('login', function() {
 	return View::make('login');
+});
+
+Route::get('logout', function() {
+	Auth::logout();
+	return Redirect::to('login');
 });
 
 Route::post('login', function() {
@@ -31,3 +29,13 @@ Route::post('login', function() {
 		echo "That didn't work";
 	}
 });
+
+/*-------------------------------------------------------------------------
+| RESTful resource controllers
+|------------------------------------------------------------------------*/
+
+Route::resource('api/users', 'UserController');
+Route::resource('api/programs', 'ProgramController');
+Route::resource('api/drills', 'DrillController');
+Route::resource('api/groups', 'GroupController');
+Route::resource('api/results', 'ResultController');
--- app/views/login.php
+++ app/views/login.php
@@ -1,18 +1,17 @@
 <!doctype html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Dialect Kuntjänst - Rapportsida</title>
-	<link rel="stylesheet" href="assets/css/style.css">
-</head>
-<body class="login">
-	<div class="login-form">
-		<h1>Logga in</h1>
-		<form method="post" action="<?php URL::to('login'); ?>">
-			<input type="text" name="email" id="email">
-			<input type="password" name="password" id="password">
-			<input type="submit">
-		</form>
-	</div>
-</body>
+	<head>
+		<meta charset="UTF-8">
+		<title>Xplode - Logga in</title>
+	</head>
+	<body class="login">
+		<div class="login-form">
+			<h1>Logga in</h1>
+			<form method="post" action="<?php URL::to('login'); ?>">
+				<input type="text" name="email" id="email">
+				<input type="password" name="password" id="password">
+				<input type="submit">
+			</form>
+		</div>
+	</body>
 </html>
