be3fccba1c403fdf1d5abf48ce950c8047e34400
Hampus Persson <hampus@hampuspersson.se>
2013-08-22 13:54:23 +0200

    Fixing Result model
    Adjusting fillable columns

--- app/models/Result.php
+++ app/models/Result.php
@@ -4,5 +4,5 @@ class Result extends Eloquent {
 
 	protected $table = 'results';
 
-	protected $fillable = array('user_id', 'drill_id', 'program_id', 'date', 'reps');
+	protected $fillable = array('user_id', 'drill_id', 'program_id', 'weight', 'reps');
 }
