76ded5063774ea3f7c33f6ffd298a5320b2683fd
Hampus Persson <hampus@hampuspersson.se>
2013-10-15 22:05:51 +0200

    Adding apple-touch-icon

--- app/views/home.php
+++ app/views/home.php
@@ -2,7 +2,10 @@
 <html lang="en" data-ng-app="xplodeApp">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-title" content="Xplode">
+	<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 	<title>Xplode your performance</title>
 	<link rel="stylesheet" href="assets/css/style.css">
 	<script src="http://code.angularjs.org/1.2.0-rc.2/angular.min.js"></script>
