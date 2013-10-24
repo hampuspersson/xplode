0b79a91f0ee9f40346ccfc70f987ec574b1fa860
Hampus Persson <hampus@hampuspersson.se>
2013-10-24 14:10:04 +0200

    Updating login screen

--- app/views/login.php
+++ app/views/login.php
@@ -8,14 +8,22 @@
 		<link rel="apple-touch-icon" href="/apple-touch-icon.png" />
 		<title>Xplode - Logga in</title>
 		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="http://i.icomoon.io/public/temp/cd2d187c8b/UntitledProject1/style.css">
 	</head>
 	<body class="purple-page">
-		<section class="login-form">
-			<h1>Logga in</h1>
+		<img src="assets/images/xplode-logo.png" alt="The Xplode logo" class="logo__large-centered">
+		<section class="purple-form">
 			<form method="post" action="<?php URL::to('login'); ?>">
-				<input type="text" name="email" id="email" placeholder="Email">
-				<input type="password" name="password" id="password" placeholder="Lösenord">
-				<input type="submit" value="Logga in">
+				<div class="input-group">
+					<label for="email" class="purple-form__label icon-user"><span class="visuallyhidden">Användare</span></label>
+					<input type="text" class="purple-form__text user" name="email" id="email" placeholder="Email">
+				</div>
+				<div class="input-group">
+					<label for="password" class="purple-form__label icon-lock"><span class="visuallyhidden">Lösenord</span></label>
+					<input type="password" class="purple-form__text" name="password" id="password" placeholder="Lösenord">
+				</div>
+				<input type="submit" class="purple-form__submit" value="Logga in">
+				<a href="#" class="purple-form__secondary">Glömt lösenordet?</a>
 			</form>
 		</section>
 	</body>
